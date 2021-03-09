e7669e84738a9131771bdc6d49ab66a5e510377e
Barbara Peric <b.peric10@gmail.com>
2021-03-09 14:39:12 +0100

    layout and header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from './components/layout';
 
 export default () => {
   return (
-    <>
+    <Layout>
       <h2>About me</h2>
       <p>This is my personal website</p>
       <Link to="/">&larr; back to home</Link>
-    </>
+    </Layout>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from './components/layout';
 
 export default () => {
   return (
-    <>
+    <Layout>
       <h2>Hello Frontend Masters!</h2>
       <Link to="/about">Learn more about me &rarr;</Link>
-    </>
+    </Layout>
   );
 };
